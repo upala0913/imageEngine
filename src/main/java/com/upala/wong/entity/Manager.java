ed82c94dca0187ccea2c272767a1d62778b62493
王鹏 <upala178421@163.com>
2019-11-06 19:20:41 +0800

    新增管理员实体类

---
+++ src/main/java/com/upala/wong/entity/Manager.java
@@ -0,0 +1,22 @@
+package com.upala.wong.entity;
+
+import lombok.Data;
+
+/********************************
+ *  @program imageEngine
+ *  @author upala
+ *  @version 0.0.1
+ *  @since 2019-11-06 19:18
+ *  @description
+ ********************************/
+
+@Data
+public class Manager {
+
+    private Integer id;
+    private String userName;
+    private String userPass;
+    private String mobile;
+    private String email;
+
+}
